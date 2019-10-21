e8665086bb95e0c6d0bfee92fbbc65b6f0fdec7b
Sarthak Rijal <rijals@uw.edu>
2019-10-20 23:54:01 -0400

    First screen

--- snake.py
+++ snake.py
@@ -1,2 +1,33 @@
-LADOOOOO
+import pygame
 
+
+pygame.init()
+
+#arbritary screen size and setup 
+size = (2255, 1360)
+
+screen = pygame.display.set_mode(size)
+grid_color = (192,192,192)
+
+#FPS
+clock = pygame.time.Clock()
+FPS = 30
+
+
+
+def play():
+
+    while True:
+     
+        #event listner
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+          
+
+
+    
+        clock.tick(FPS)
+
+play()
