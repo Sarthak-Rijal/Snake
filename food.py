2f4355333a405bbe22a4c9be3880da16d2c47e5c
aka334 <ayushkarki92@gmail.com>
2019-11-02 02:35:46 -0400

    Working

---
+++ food.py
@@ -0,0 +1,17 @@
+import pygame 
+import sys
+
+pygame.init();
+class food():
+    def __init__(self, screen,size = [20, 20], color = (128,0,0), pos = [15, 15]):
+        self.screen = screen
+        self.width = width
+        self.length = length
+        self.color = color
+        self.posx = posx
+        self.posy = posy
+
+   def drawfood(self, screen, color, pos):
+        pygame.draw.rect(screen, RED,())
+
+    
